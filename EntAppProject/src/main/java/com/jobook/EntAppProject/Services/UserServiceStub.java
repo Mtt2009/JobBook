da4e1e52228850e3420014283ecafc7a7765da24
Yusuf Said <saidyf@mail.uc.edu>
2019-10-18 00:34:51 -0400

    Code Review Changes - update

--- EntAppProject/src/main/java/com/jobook/EntAppProject/Services/UserServiceStub.java
+++ EntAppProject/src/main/java/com/jobook/EntAppProject/Services/UserServiceStub.java
@@ -42,7 +42,17 @@ public class UserServiceStub implements IUserService {
 	}
 
 	public User getUserInfoById(int userId) {
-		User user = new User(null, "Stub", "Testing", "Test", "Password", "Test@Test.com", userId, null, "C");
+		User user = null;
+		switch (userId) {
+			case 0: {
+				user = new User(null, "Stub", "Testing", "Test", "Password", "Test@Test.com", 0, null, "C");
+				break;
+			}
+			case 1: {
+				user = new User(null, "Stub", "Testing", "TestEmployee", "Password", "TestEmployee@Test.com", 1, null, "E");
+				break;
+			}
+		}
 		Address address = new Address("Cincinnati", "Ohio", "45206", "123 Test Rd.", "", 0);
 		user.setAddress(address);
 		user.setAddressId(address.getId());
