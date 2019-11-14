388a8c8d632464477ff364fce0bf6d786546c11f
Samuel Curry <samc735@gmail.com>
2019-11-13 19:00:58 -0500

    removed unused imports

--- EntAppProject/src/main/java/com/jobook/EntAppProject/model/Address.java
+++ EntAppProject/src/main/java/com/jobook/EntAppProject/model/Address.java
@@ -1,7 +1,5 @@
 package com.jobook.EntAppProject.model;
 
-import org.springframework.stereotype.Component;
-
 public class Address {
 	private String city, state, zip, line1, line2;
 	private Integer id;
--- EntAppProject/src/main/java/com/jobook/EntAppProject/model/Customer.java
+++ EntAppProject/src/main/java/com/jobook/EntAppProject/model/Customer.java
@@ -2,34 +2,32 @@ package com.jobook.EntAppProject.model;
 
 import java.util.List;
 
-import org.springframework.stereotype.Component;
-
 public class Customer extends User {
 	private String preferredPaymentMethod;
 	private List<Job> orderHistory, currentOrders;
 	private List<Review> reviews;
-	
+
 	public Customer(Address address, String firstName, String lastName, String userName, String password, String email,
 			Integer id, Integer addressId, String userType) {
 		super(address, firstName, lastName, userName, password, email, id, addressId, userType);
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public void addReview(Review review) {
 		this.reviews.add(review);
 	}
-	
+
 	public void addNewJob(Job job) {
 		this.currentOrders.add(job);
 	}
-	
+
 	public void moveJobToHistory(Job job) throws Exception {
 		int index = currentOrders.indexOf(job);
-		
+
 		if(index == -1) {
 			throw new Exception("Job does not exist in current orders list.");
 		}
-		
+
 		orderHistory.add(job);
 		currentOrders.remove(index);
 	}
@@ -65,5 +63,5 @@ public class Customer extends User {
 	public void setReviews(List<Review> reviews) {
 		this.reviews = reviews;
 	}
-	
+
 }
